ffb642a0e34a04049cfc33b2bd5209d40b7bbf41
Snoopy IUP <lkk324437@gmail.com>
2026-04-17 16:18:15 +0000

    Latest build deployed.

---
+++ course/lunr-pretext-search-index.js
@@ -0,0 +1,68 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "syllabus",
+  "level": "1",
+  "url": "syllabus.html",
+  "type": "Section",
+  "number": "",
+  "title": "Syllabus",
+  "body": " Syllabus   Instructors reserve the right to modify this syllabus as necessary. Only work assigned by, and submitted to, the instructor of record will count toward the final grade in this course.     Course Information     Instructor  Yuju Kuo    Office  STRGT 314    Email   yjkuo@iup.edu .    Office Hours  Monday 12:30 PM - 1:30 PM  Tuesday 1:00 PM - 2:00 PM, 5:00 PM - 6:00 PM  Wednesday 12:30 PM - 2:30 PM  Thursday 1:00 PM - 2:00 PM (via Zoom)   Important : I encourage you to attend office hours, and I am happy to schedule appointments outside of the regular hours. I am available by email and will respond within 24 hours—usually much sooner—on weekdays. You can also make appointments through     Class meets  Section 001   Tuesday 11:10 AM-12:25 PM, STABL 102B    Thursday 11:10 AM-12:25 PM, STRGT 229     Section 002   Tuesday 2:20 PM-3:35 PM, STABL 102B    Thursday 2:20PM-3:35 PM, STRGT 229       Course Description  Analyzes and interprets numerical, graphical, and algebraic representations to model complex situations using mathematical structures and increase problem-solving skills. Focuses on polynomial, exponential and logarithmic functions, and basic right triangle trigonometry.    Prerequisite  MATH 100 or appropriate placement test score or permission of the MACS chairperson. Note: May not be taken after successfully completing either a calculus course or MATH 110 without written MACS Department chairperson approval.    Textbook and course materials     Miller: College Algebra and Trigonometry, 2nd Ed. (McGraw Hill)    ALEKS 360 (https:\/\/www.aleks.com\/sign_up)    ALEKS Support: https:\/\/mhedu.force.com\/aleks\/s\/alekscontactsupport          Assessments and Grades   Learning Outcomes  By the end of this course, you should be able to:   Use graphical and\/or analytic methods to demonstrate an understanding of properties of functions and operations on functions.    Apply algebraic, numerical and \/or graphical problem solving techniques to solve polynomial, rational, exponential, and\/or logarithmic equations or inequalities.    Construct and interpret mathematical models of real world situations using polynomial, rational, exponential, and\/or logarithmic functions.       Assessment Categories     Category   Weight    Grade   Percentage     In-Class Activities   12%    A  90-100%     Weekly Modules (ALEKS)   8%    B  80-89%     Weekly Homework (ALEKS)   8%    C  70-79%     Bi-Weekly Quizzes (ALEKS)   7%   D   55-69%     3 Midterm Exams   45%    F  below 55%     Final Exam   20%          Course Information   ALEKS ALEKS is a Web-based, artificially intelligent assessment and learning system that uses adaptive questioning to quickly and accurately determine exactly what a student knows, and doesn't know, in a course. ALEKS then instructs the student on the topics he or she is most ready to learn.    Initial Knowledge Check: Must complete first before you can access other contents in the course. This will help the system to determine your background and adjust the level of content for you to start and practice.    Prerequisite Review Skills (Adaptive learning mode): This module is to review essential mathematics skills for the course.     Syllabus Quiz: The syllabus quiz consists of questions from the syllabus. You must receive 100% to activate all bi-weekly quizzes. Three attempts are allowed initially and the last question needs to be graded manually. Contact the instructor asap if you don’t reach 100% within 3 attempts.     Weekly Modules (Adaptive learning mode): The modules will cover material for each week. Most are due on Wednesday except the first week. Must obtain at least 60% to activate the homework for that week.    Weekly Homework: To activate the weekly homework, you must obtain at least 60% on the associated weekly module. Deadlines of weekly homework are mostly on Saturday. Each question in the weekly homework allows the maximum of 2 attempts and it must be submitted within 40 minutes after starting it. E-text, examples, or example explanation are accessible.     Bi-Week Quizzes: Quizzes are scheduled every 2 weeks, which contain accumulated contents from the most recent 2 weeks. It provides regular reviews to prepare for exams. You must receive 100% on the syllabus quiz to activate all quizzes. Each quiz allows two attempts within a 4-day period (Friday-Monday) with a time limitation of 30 minutes. Late submissions are allowed with the daily deduction of 33%.    Three Midterm Exams: Exams will be proctored with a maximum of 85 minutes in Stabley 102B. The exams will on Feb. 17, Mar. 24, and Apr. 21.  Section 001: 11:05 AM-12:30 PM   Section 002: 2:15 PM- 3:40 PM   If you have time conflict, please contact the instructor during the first week.    Final Exam: The Final will be proctored with a maximum of 120 minutes.  Section 001: 10:20 AM - 12:20 PM on May 5   Section 002: 12:40 PM - 2:40 PM on May 5         In-Class Activities: During class periods, we will use various approaches to enhance the understanding of topics for each week. In-class activities may contain, but not limited to, paper quizzes (individual), discussions, applications, and games. Make-ups are allowed for individual activities if they are done before the following class. Make-ups are not allowed for group activities.    Course Structure: This will not be a traditional lecture style class. You will spend your class time learning in various approaches. In general, you should expect   On Tuesday, we may have short lectures or class activities to learn difficult topics in the module, or you may spend some time work through ALEKS to finish up the weekly module.    On Thursday, you will be in a regular classroom and we will review and practice topics that you and other students find challenged, then we will wrap up the topics for that week by working on additional problems, particularly word problems.   Individual or group work may be collected for grading as In-class activities on either days. You can make up individual activities before the following class. Make-ups for group activities are not allowed.       D2L: Course syllabus and schedule will be posted on D2L. Overall grade for each category will also be posted on D2L. Keep eyes on the announcements and your IUP email.      Expectations    You are expected to be in class every day that it is scheduled, and you will be expected to use this time to work on only the course material. You will also be required to spend additional time outside of class to complete assignments.    You will need a notebook. Problems should be done on paper, and the answers then entered in ALEKS; a neat notebook, complete with date and problem headings, will help you a great deal when it is time to study for exams or seek helps.    IUP photo identification will be required for each of 3 midterm exams and the final.    Cell phones should not be used\/visible during class time. Please take care of any personal business outside of class time. Visible devices during exams will be considered a violation of IUP’s academic integrity policy.    You must contact the instructor (via email: yjkuo@iup.edu) within 24 hours of missing an exam and be prepared to provide documentation. Makeups are given at the discretion of the instructor and only in the case of verified medical r other documented emergencies.        Tentative Schedule    Week 1-Week 3 Graphs, Equations and Inequalities    Week 4- Week 6 Functions    Week 7- Week 9 Polynomial and Rational Functions    Week 10-Week 12 Exponential and Logarithmic Functions    Week 13- Week 14 Basic Trigonometry      Important Dates     Weekly Module  Mostly Due Wednesday   Weekly Homework  Mostly Due Saturday    Bi-Weekly Quiz  Due on every other Monday    Exam 1  February 17    Exam 2  March 24    Exam 3  April 21    Final Exam     Section 001: May 5, 10:20 AM- 12:20 PMM   Section 002: May 7, 12:40 PM- 2:40 PM          University Policies   Academic Integrity  All students are required to abide by the University Policy on Academic Integrity. YOU MUST DO ALL YOUR OWN WORK. See: and the section on Academic Policies and Academic Integrity: Indiana University of Pennsylvania is committed to the fundamental values of academic integrity. Academic integrity means honesty and responsibility in scholarly endeavors and behaviors; it means that all academic work should be the result of an individual's own effort. Academic assignments help students learn and allow them to exhibit this earning. Grades are an assessment of the extent to which learning has been demonstrated in assignments. Therefore, academic work and grades should be the result of a student’s own understanding and effort. All members of the IUP community-including students, faculty, and staff-are responsible for maintaining academic integrity, which includes knowing what IUP's academic integrity policies are and being able to identify academic misconduct. Academic misconduct includes any action which improperly impacts the assessment or representation of a student's academic achievement. Academic misconduct may result in disciplinary action, including expulsion from the University. Students who do not do their own work can be subject to penalty, including but not limited to zero grades on affected assignments and\/or referral to the University for academic integrity violations.    Department of Disability Advising and Access (D2A2)  Indiana University of Pennsylvania provides reasonable accommodations to students with disabilities who have self-identified and been approved by the Department for Disability Access and Advising (D2A2). If you have any kind of disability, whether apparent or non-apparent, learning, emotional, physical, or cognitive, chronic or short-term, please make an appointment to meet with me as soon as possible in order to discuss your accommodations and your access needs. All discussions will remain confidential. You must be registered with the Department for Disability Access and Advising (D2A2), and requested them to provide your accommodation letter to me, before I can accommodate you. If you have not yet contacted D2A2, and would like to request accommodations or have questions, you can make an appointment by emailing (preferred) D2A2 at or by calling 724-357-4067. The office is located in Pratt Hall, Room 216, 201 Pratt Drive. All services are confidential.    Title IX  Indiana University of Pennsylvania and its faculty are committed to assuring a safe and productive educational environment for all students. In order to meet this commitment and to comply with Title IX of the Education Amendments of 1972 and guidance from the Office for Civil Rights, the University requires faculty members to report incidents of sexual violence shared by students to the University's Title IX Coordinator. The only exceptions to the faculty member's reporting obligation are when incidents of sexual violence are communicated by a student during a classroom discussion, in a writing assignment for a class, or as part of a University-approved research project. Faculty members are obligated to report sexual violence or any other abuse of a student who was, or is, a child (a person under 18 years of age) when the abuse allegedly occurred to the Department of Human Services (1-800-932-0313) and University Police (724-357-2141). Information regarding the reporting of sexual violence and the resources that are available to victims of sexual violence is set forth at:     "
+},
+{
+  "id": "sec-assessments-4-2-2-1",
+  "level": "2",
+  "url": "syllabus.html#sec-assessments-4-2-2-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "In-Class Activities: "
+},
+{
+  "id": "sec-assessments-4-2-3-1",
+  "level": "2",
+  "url": "syllabus.html#sec-assessments-4-2-3-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Course Structure: "
+},
+{
+  "id": "sec-assessments-4-2-4-1",
+  "level": "2",
+  "url": "syllabus.html#sec-assessments-4-2-4-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "D2L: "
+},
+{
+  "id": "handouts",
+  "level": "1",
+  "url": "handouts.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Handouts",
+  "body": " Handouts     "
+},
+{
+  "id": "homework",
+  "level": "1",
+  "url": "homework.html",
+  "type": "Chapter",
+  "number": "",
+  "title": "Homework",
+  "body": " Homework    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
